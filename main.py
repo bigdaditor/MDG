65548440b8423b57104c1c6bc05b40cd85e4801b
bigdaditor <bigdaditor1015@gmail.com>
2025-01-06 16:25:55 +0900

    랜덤 로그 생성 쓰레드 실행 추가

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+import threading
 
+from fastapi import FastAPI
+import generater
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello MDG"}
+
+def start_server():
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+# 메인 서버 실행
+def main():
+    start_server()
+
+def start_log_thread():
+    # 랜덤 로그 생성 쓰레드
+    log_thread = threading.Thread(target=generater.generate_log, daemon=True)
+    log_thread.start()
+
+if __name__ == "__main__":
+    main()
+    start_log_thread()
